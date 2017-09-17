67c13c138a573e5b7e4449a4a0b884adbf44d402
Anton Bossenbroek <anton.bossenbroek@me.com>
2017-09-16 20:04:10 -0500

    Adds bigger step between desired output lanes
    The points on the desired output state are now more spaced out.

--- src/main.cpp
+++ src/main.cpp
@@ -164,7 +164,7 @@ int main() {
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
 
-          for (double i = 0; i < 100; ++i) {
+          for (double i = 0; i < 100; i += 5) {
             next_x_vals.push_back(i);
             next_y_vals.push_back(polyeval(coeffs, i));
           }
